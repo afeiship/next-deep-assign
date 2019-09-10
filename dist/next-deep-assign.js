f4b5e68cbf333be10b67162fb36d9d52018519a9
afeiship <1290657123@qq.com>
2019-09-10 22:37:24 +0800

    add: intial

--- dist/next-deep-assign.js
+++ dist/next-deep-assign.js
@@ -1,3 +1,11 @@
+/*!
+ * name: next-deep-assign
+ * url: https://github.com/afeiship/next-deep-assign
+ * version: 1.0.0
+ * date: 2019-09-10T14:37:20.034Z
+ * license: MIT
+ */
+
 (function() {
   var global = global || this || window || Function('return this')();
   var nx = global.nx || require('next-js-core2');
@@ -37,3 +45,5 @@
     module.exports = nx.deepAssign;
   }
 })();
+
+//# sourceMappingURL=next-deep-assign.js.map
--- test/debug.js
+++ test/debug.js
@@ -1,2 +1,26 @@
 var nx = require('next-js-core2');
 require('../src/next-deep-assign');
+
+var obj1 = {
+  lineHeight: 26,
+  left: 'center',
+  style: {
+    items: ['a'],
+    fontSize: 24,
+    fill: '#333',
+    fontWeight: 500
+  }
+};
+var obj2 = {
+  lineHeight: 100,
+  left: '30%',
+  right: '40%',
+  style: {
+    items: [1, 2, 3],
+    fontFamily: 'Arial'
+  }
+};
+
+var result = nx.deepAssign(obj1, obj2);
+
+console.log('rs:->', result);
